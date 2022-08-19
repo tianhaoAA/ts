9071c6b35b560305d7e45e464e7402bc4aeec911
tianhao <1334750953@qq.com>
2022-08-19 19:45:10 +0800

    test

--- 14-泛型/app.ts
+++ 14-泛型/app.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: tianhao 1334750953@qq.com
+ * @Date: 2022-08-19 17:03:42
+ * @LastEditors: tianhao 1334750953@qq.com
+ * @LastEditTime: 2022-08-19 19:13:32
+ * @FilePath: \TS\14-泛型\app.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // 函数泛型
 
 // 1 函数名字后面跟一个<参数名> 参数名可以随便写
@@ -29,3 +37,24 @@ function fn<T>(age: T): T {
 let result: MyInter<number> = fn;
 
 result(1223);
+
+// 对象字面量泛型
+
+let foo: { <T>(age: T): T };
+
+foo = function fn2<T>(age: T): T {
+  return age;
+};
+foo(123);
+
+// 泛型约束
+
+interface Leng {
+  length: number;
+}
+
+function getLength<T extends Leng>(arg: T) {
+  return arg.length;
+}
+
+getLength<string>("123");
